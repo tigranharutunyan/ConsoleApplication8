34a7a6651feadc32808a3b6056dda81630fdd470
4-2 <4-2@13-2-PC>
2023-09-29 11:52:48 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void HanoTower(int m, char A, char B, char C)
+{ 
+    
+    
+    if (m > 0)
+    {
+        HanoTower(m - 1, A, C, B);
+        cout << m << A << "   from" << "   to" << B << endl;    
+        HanoTower(m - 1, C,B,A );
+    }    
+}
+int main()
+{
+   
+    int towernumber;
+    cout << "Enter your towernumber";
+    cin >> towernumber;
+    HanoTower(towernumber, 'A', 'B', 'C');
+    cout << pow(2, towernumber) - 1 << endl;
+}
+
